b6bb6f54868f2eee93dcaaf1bf33792474026100
Sarah <rainbee2214@gmail.com>
2015-10-06 09:36:05 -0300

    Removed d3 stuff.

---
+++ python/testAnalytics.py
@@ -0,0 +1,17 @@
+import sys
+import json
+
+import csv
+from enum import Enum
+
+class Type(Enum):
+    Groups = ['Groups', True, True, True, True]
+    Users = ['Users', True, True, True, True]
+
+def main(argv):
+
+
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
