8a9153fd43b94ed98865d32b81af538d6a412069
Glavin Wiechert <glavin.wiechert@gmail.com>
2015-10-08 22:15:41 -0300

    Renamed automigrate to autoupdate for auto-update-tables script

--- server/auto-update-tables.js
+++ server/auto-update-tables.js
@@ -1,6 +1,6 @@
 /*
  * See https://docs.strongloop.com/display/public/LB/Creating+database+tables+for+built-in+models
- * 
+ *
  */
 
 var server = require('./server');
@@ -8,10 +8,10 @@ var ds = server.dataSources.db;
 //var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
 var modelConfig = require('./model-config.json')
 var lbTables = Object.keys(modelConfig)
-    .filter(function(key) { 
-        return key !== '_meta'; 
+    .filter(function(key) {
+        return key !== '_meta';
     });
-ds.automigrate(lbTables, function(er) {
+ds.autoupdate(lbTables, function(er) {
       if (er) throw er;
         console.log('Looback tables [' + lbTables + '] created in ', ds.adapter.name);
           ds.disconnect();
